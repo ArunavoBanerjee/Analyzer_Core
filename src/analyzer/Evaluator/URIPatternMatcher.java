051c129579e111b95f42c23423acf33dd8067ff1
srvcs <srvcs@LAPTOP-U5VQTMV1>
2021-07-26 14:32:04 +0530

    Ana Release Prep 25.07.2021 V0.3

--- src/analyzer/Evaluator/URIPatternMatcher.java
+++ src/analyzer/Evaluator/URIPatternMatcher.java
@@ -1,5 +1,7 @@
 package analyzer.Evaluator;
 
+import java.util.ArrayList;
+
 public class URIPatternMatcher {
 private static volatile URIPatternMatcher instance = null;
 	
@@ -16,6 +18,14 @@ private static volatile URIPatternMatcher instance = null;
 		return instance;
 	}
 	
-	
+	public boolean uri_matcher(String fieldValue, ArrayList<String> patternProp) throws Exception{
+		String matchType = patternProp.get(1);
+		switch(matchType) {
+		case "liveExists":
+			return ProdDupChecker.getInstance().getResult(fieldValue);
+		default:
+				throw new Exception("URI validation is currently only supported for Live Checking.\nPlease refer Analyzer Manual for more details.");
+		}
+	}
 	
 }
