8f3b92c04e0f3619d3d537d09af9b691d5dcf65e
Arunavo <Arunavo@Arunavo-PC>
2021-12-30 14:47:51 +0530

    ANA Draft Release 30.12.2021 d0.2

--- src/analyzer/Base/Analyzer.java
+++ src/analyzer/Base/Analyzer.java
@@ -64,9 +64,9 @@ public class Analyzer {
 		Splitter.sourceList = prop.getProperty("sourceFile").split(",");
 		if(prop.getProperty("csvMultivalueSep") != null)
 		Splitter.csvMultivalueSep = prop.getProperty("csvMultivalueSep","||").strip();
-		if (prop.getProperty("MatchedDataPath") != null)
+		if (prop.getProperty("MatchedData") != null)
 			Splitter.dest_matched = Splitter.rootLocation + "/" + prop.getProperty("targetFileMatched").strip();
-		if (prop.getProperty("UnMatchedDataPath") != null)
+		if (prop.getProperty("UnMatchedData") != null)
 			Splitter.dest_unmatched = Splitter.rootLocation + "/" + prop.getProperty("targetFileUnMatched").strip();
 		if (prop.getProperty("reportDestination") != null)
 			Splitter.reportDest = Splitter.rootLocation + "/" + prop.getProperty("reportDestination").strip();
